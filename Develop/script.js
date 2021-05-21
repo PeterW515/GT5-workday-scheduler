f4eaa1e41bdc8a863a9528b1e870eb5b89b6b520
Peter Weigman <phweigman@gmail.com>
2021-05-21 16:26:56 -0400

    completed most of the stying. Functionality and comments still needed.

--- Develop/script.js
+++ Develop/script.js
@@ -11,32 +11,53 @@ let hoursPast = currentTime-9;
 
 for(let i =0; i <9; i++){
     let timeblock = document.createElement('div');
-    timeblock.id = 'timeblock'+i;
-    timeblock.className = 'row time-block';
-
     let timeblockHour = document.createElement('div');
-    timeblockHour.id = 'timeblockHour'+i;
-    timeblockHour.className = 'hour';
-    timeblockHour.innerText = 9+i;
-
-    let timeblockMain = document.createElement('div');
-    timeblockMain.id = 'timeblockMain'+i;
-    timeblockMain.className = 'time-block';
-    timeblockMain.innerText = 'test';
-
+    let timeblockMain = document.createElement('textarea');
     let timeblockSave = document.createElement('div');
+    let saveBtn = document.createElement('button');
+    let saveBtnLogo = document.createElement('i');
+
+    timeblock.id = 'timeblock'+i;
+    timeblockHour.id = 'timeblockHour'+i;
+    timeblockMain.id = 'timeblockMain'+i;
     timeblockSave.id = 'timeblockSave'+i;
-    timeblockSave.className = 'saveBtn fas fa-save time-block';
+    saveBtn.id = 'saveBtn' + i;
+    saveBtnLogo.id = 'saveBtnLogo' + i;
+
+
+    timeblock.className = 'row time-block';
+    timeblockHour.className = 'hour pt-2 col-1 d-flex justify-content-end';
+    timeblockMain.className = 'text-left pt-2 col-10';
+    timeblockSave.className = 'saveBtn col-1 d-flex justify-content-center';
+    saveBtnLogo.className = 'fas fa-save';
+    saveBtn.className = 'align-self-center'
+
+    if(i+9 === moment().hour()) {
+        timeblockMain.className = 'present text-left pt-2 col-10';
+    } else if (i+9 < moment().hour()) {
+        timeblockMain.className = 'past text-left pt-2 col-10';
+    } else {
+        timeblockMain.className = 'future text-left pt-2 col-10';
+    }
+
+    if (i<3){
+        timeblockHour.innerText = 9+i+' am';
+    } else if(i===3) {
+        timeblockHour.innerText = (9+i) + ' pm';
+    } else {
+        timeblockHour.innerText = (9+i)-12 + ' pm';
+    }
+
+    saveBtn.appendChild(saveBtnLogo);
+    timeblockSave.appendChild(saveBtn);
 
     timeblock.appendChild(timeblockHour);
     timeblock.appendChild(timeblockMain);
     timeblock.appendChild(timeblockSave);
 
 
-
     timeblockContainer.appendChild(timeblock);
 }
 
-if(hoursPast===0){
 
-}
+
