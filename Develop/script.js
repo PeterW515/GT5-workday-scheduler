a01f8c95ca3371d830d862e7447f2332ddcba897
Peter Weigman <phweigman@gmail.com>
2021-05-21 17:00:41 -0400

    added functionality and roughly completed application. Testing and tweaks still needed.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,25 @@
 const currentDateElement = document.getElementById('currentDay');
 const timeblockContainer = document.getElementById('timeblock-container');
 
-let currentDate = moment().format('dddd, MMMM Do');
+
+
+let currentDate = moment().add(1,'days').format('dddd, MMMM Do');
+
+if(localStorage.getItem('currentDate')){
+    if (JSON.parse(localStorage.getItem('currentDate')) != currentDate){
+        wipeCalender();
+    }
+}  else {
+    localStorage.setItem('currentDate',JSON.stringify(currentDate));
+}
+
 
 currentDateElement.innerText = currentDate;
 
 
 let currentTime = moment().hour();
+
+
 let hoursPast = currentTime-9;
 
 for(let i =0; i <9; i++){
@@ -57,7 +70,36 @@ for(let i =0; i <9; i++){
 
 
     timeblockContainer.appendChild(timeblock);
+
+
+    //create event listener for the button
+
+    saveBtn.addEventListener('click',saveFunc);
 }
 
+let localStorageArray = [];
+if(localStorage.getItem('eventArray')){
+    localStorageArray = JSON.parse(localStorage.getItem('eventArray'));
+    populateCalendar();
+}
 
+function saveFunc(e) {
+    let timeblockNum = e.target.id.charAt(e.target.id.length-1);
+    localStorageArray[timeblockNum] = document.getElementById('timeblockMain'+timeblockNum).value;
+    localStorage.setItem('eventArray',JSON.stringify(localStorageArray));
+}
 
+function populateCalendar(){
+    for(let i =0;i<9;i++){
+        if(localStorageArray[i]){
+            document.getElementById('timeblockMain'+i).value = localStorageArray[i]
+        }
+    }
+}
+
+function wipeCalender() {
+    for(let i = 0; i <9; i++){
+        document.getElementById('timeblockMain'+i).value='';
+        localStorage.setItem('eventArray',null);
+    }
+}
